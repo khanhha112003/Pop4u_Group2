e95471a5eee49e8394cc1d6238e4c7ce0afdb692
thalusooissm <lucnt21411@st.uel.edu.vn>
2023-11-21 20:09:22 +0700

    about-us page

---
+++ nav-bar/script.js
@@ -0,0 +1,10 @@
+// Add a class to the navbar when scrolling down
+window.addEventListener('scroll', function() {
+    var navbar = document.getElementById('navbar');
+    if (window.scrollY > 0) {
+      navbar.classList.add('scrolled');
+    } else {
+      navbar.classList.remove('scrolled');
+    }
+  });
+  
