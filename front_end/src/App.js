b891c7ddbcc55fcd13fbaaa0d5dc941d3f57c453
ThuyBumr <anhthuylqd@gmail.com>
2023-12-18 12:08:31 +0700

    1

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,8 +1,12 @@
-
+import Footer from './components/Footer/Footer';
+import Navbar from './components/Navbar/Navbar';
 import { HomePage } from './screens/HomePage'
 function App() {
   return (
     <div className="App">
+       <Navbar></Navbar>
+        <HomePage></HomePage>
+      <Footer></Footer>
       <HomePage></HomePage>
     </div>
   );
--- front_end/src/screens/HomePage.js
+++ front_end/src/screens/HomePage.js
@@ -1,10 +1,8 @@
-import Navbar from '../components/Navbar/Navbar'
-import Footer from '../components/Footer/Footer';
+
 function HomePage() {
   return (
     <div className="App">
-      <Navbar></Navbar>
-      <Footer></Footer>
+      { /* TODO: fill here*/ }
     </div>
   );
 }
