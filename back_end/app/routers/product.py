9d5e14b108d72d87507fa6c576da2aa2dbab4275
ThuyBumr <104961603+ThuyBumr@users.noreply.github.com>
2023-12-21 23:50:43 +0700

    Add files via upload

--- back_end/app/routers/product.py
+++ back_end/app/routers/product.py
@@ -4,10 +4,10 @@ from serializer.user_serializer import userResponseEntity
 
 from database import db
 from oauth2 import get_current_active_user
-from schemas import TokenData, User
+from schemas import Product, Token, ProductReview
 router = APIRouter()
 
-@router.get('/me', response_model=User)
+@router.get('/me', response_model=Token)
 def get_me(user_id: str = Depends(get_current_active_user)):
     user = userResponseEntity(db["Users"].find_one({'_id': ObjectId(str(user_id))}))
     return {"status": "success", "user": user}
