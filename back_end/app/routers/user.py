6d75a677a512068973d5cf9fe16cb192d06ad99d
ThuyBumr <104961603+ThuyBumr@users.noreply.github.com>
2023-12-21 23:38:42 +0700

    Create user.py

---
+++ back_end/app/routers/user.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter, Depends
+from bson.objectid import ObjectId
+from serializer.user_serializer import userResponseEntity
+
+from database import db
+from oauth2 import get_current_active_user
+from schemas import TokenData, User
+router = APIRouter()
+
+@router.get('/me', response_model=User)
+def get_me(user_id: str = Depends(get_current_active_user)):
+    user = userResponseEntity(db["Users"].find_one({'_id': ObjectId(str(user_id))}))
+    return {"status": "success", "user": user}
