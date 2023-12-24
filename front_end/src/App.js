1ff7e0ac95d69d53405e99de44969cd5a8e11f4d
khanhha112003 <102938158+khanhha112003@users.noreply.github.com>
2023-12-24 23:04:11 +0700

    product detail

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,11 +1,12 @@
 import Footer from './components/Footer/Footer';
 import Navbar from './components/Navbar/Navbar';
-import { HomePage } from './screens/HomePage'
+// import { HomePage } from './screens/HomePage/HomePage'
+import { ProductDetail } from './screens/ProductDetail'
 function App() {
   return (
     <div className="App">
        <Navbar></Navbar>
-       <HomePage></HomePage>
+       <ProductDetail></ProductDetail>
       <Footer></Footer>
     </div>
   );
