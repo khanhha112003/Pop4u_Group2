a57e996645732e23de0de8aba5b76248db26be3c
xuntheo <thieuntx21411@st.uel.edu.vn>
2023-12-24 22:54:44 +0700

    chua co gi

--- front_end/src/screens/ProductPage/ProductPage.js
+++ front_end/src/screens/ProductPage/ProductPage.js
@@ -2,6 +2,7 @@ import React from "react";
 import './ProductPage.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import banner_tet from './images/banner_tet.png'
+import { ReactComponent as Star } from './images/grade.svg';
 
 function ProductPage () {
 return (
@@ -11,6 +12,23 @@ return (
         <img src={banner_tet} className='img-fuild img-thumbnail' alt='banner' />
     </div>
     
+
+    <div className="card-container">
+        <div className="col-md-4">
+            <div className="card">
+                <img src="" alt=""></img>
+                <div className="card-body">
+                    <span className="tag">Mới</span>
+                    <span className="tag">Freeship</span>
+                    <h5 className="card-title">  j-hope (BTS) 'Jack In The Box' (HOPE Edition)</h5>
+                    <div className="card-reviews"> 
+                        <Star/> <Star/> <Star/> <Star/> <Star/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <div className="container grid grid-filter-column"> 
     <div> <filtersection/></div>
     
