f5fb75ef6ef3f747081a753edd8f856d5144fcea
Le Thi Thanh Tam <thanhtamdl2701@gmail.com>
2023-12-25 23:50:22 +0700

    test commit

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,12 +1,16 @@
 import Footer from './components/Footer/Footer';
 import Navbar from './components/Navbar/Navbar';
-import { HomePage } from './screens/HomePage/HomePage'
+// import { HomePage } from './screens/HomePage/HomePage';
+import BlgLst from './screens/Blog/BlgLst/BlgLst';
+import SinglePost from './screens/Blog/SinglePost/SinglePost';
 
 function App() {
   return (
     <div className="App">
        <Navbar></Navbar>
-        <HomePage></HomePage>
+        {/* <HomePage></HomePage> */}
+        {/* <BlgLst></BlgLst> */}
+        <SinglePost></SinglePost>
       <Footer></Footer>
     </div>
   );
---
+++ front_end/src/screens/Blog/data.js
@@ -0,0 +1,37 @@
+const Blgdata = {
+    BlgHeader : [
+        {
+            Image: "/img/blog-bg.png",
+
+        }
+    ],
+
+    BlgPosts : [
+        {
+            Image:'/img/postimg',
+            name: 'Lorem ipsum dolor sit amet',
+            slug: 'Lorem ipsum dolor sit amet',
+            description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores quia vitae incidunt necessitatibus, sapiente voluptate. Nam eos accusantium ea aspernatur e recusandae obcaecati iste odit maxime, necessitatibus fuga harum consequatur.',
+        },
+        {
+            Image:'/img/postimg',
+            name: 'Lorem ipsum dolor sit amet',
+            slug: 'Lorem ipsum dolor sit amet',
+            description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores quia vitae incidunt necessitatibus, sapiente voluptate. Nam eos accusantium ea aspernatur e recusandae obcaecati iste odit maxime, necessitatibus fuga harum consequatur.',
+        },
+        {
+            Image:'/img/postimg',
+            name: 'Lorem ipsum dolor sit amet',
+            slug: 'Lorem ipsum dolor sit amet',
+            description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores quia vitae incidunt necessitatibus, sapiente voluptate. Nam eos accusantium ea aspernatur e recusandae obcaecati iste odit maxime, necessitatibus fuga harum consequatur.',
+        },
+        {
+            Image:'/img/postimg',
+            name: 'Lorem ipsum dolor sit amet',
+            slug: 'Lorem ipsum dolor sit amet',
+            description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores quia vitae incidunt necessitatibus, sapiente voluptate. Nam eos accusantium ea aspernatur e recusandae obcaecati iste odit maxime, necessitatibus fuga harum consequatur.',
+        },
+    ]
+}
+
+export default Blgdata;
