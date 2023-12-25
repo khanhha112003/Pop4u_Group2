5492beb546884a7417d30ad32036df1b17a4e872
khanhha112003 <102938158+khanhha112003@users.noreply.github.com>
2023-12-25 10:16:16 +0700

    pull

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,12 +1,15 @@
 import Footer from './components/Footer/Footer';
 import Navbar from './components/Navbar/Navbar';
-// import { HomePage } from './screens/HomePage/HomePage'
-import { ProductDetail } from './screens/ProductDetail'
+import { HomePage } from './screens/HomePage/HomePage'
+// import { ProductDetail } from './screens/ProductDetail'
+// import { Cart } from './screens/Cart'
 function App() {
   return (
     <div className="App">
        <Navbar></Navbar>
-       <ProductDetail></ProductDetail>
+       {/* <ProductDetail></ProductDetail> */}
+       {/* <Cart></Cart> */}
+       <HomePage></HomePage>
       <Footer></Footer>
     </div>
   );
