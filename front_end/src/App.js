733ae941e2517d33f6f5c01a2f7cab2895e05d68
xuntheo <thieuntx21411@st.uel.edu.vn>
2023-12-26 13:21:55 +0700

    add productpage

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -3,10 +3,12 @@ import Navbar from './components/Navbar/Navbar';
 // import { HomePage } from './screens/HomePage/HomePage'
 import { ProductPage } from './screens/ProductPage/ProductPage'
 
+
 function App() {
   return (
     <div className="App">
        <Navbar></Navbar>
+       
        <ProductPage></ProductPage>
       <Footer></Footer>
     </div>
