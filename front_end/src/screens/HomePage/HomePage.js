cd21fd7e7f5e9b2d63207c8d5a4d7ba6cf86ca2b
ThuyBumr <anhthuylqd@gmail.com>
2023-12-26 12:46:50 +0700

    for my husband work

--- front_end/src/screens/HomePage/HomePage.js
+++ front_end/src/screens/HomePage/HomePage.js
@@ -11,10 +11,23 @@ import img_product from './icons/img_product.png'
 import { ReactComponent as Star } from './icons/icon_star.svg';
 import img_blackpink from './icons/img_blackpink.png'
 import logo_blackpink from './icons/logo_blackpink.png'
+import HomepageProductItem from "../../components/HomepageProductItem/HomepageProductItem";
+
+
+// e giờ làm theo kiểu đó thì để nó hiện bao nhiêu item cũng được
+// nhưng mà để cái đó làm sau hí, giờ
+// 1- làm cho cái product detail item đẹp
+// 2- làm scroll, fix cái layout khi scroll
+// 3.1: Tìm những cái phần tử trong cái design nhìn có vẻ giống (lặp lại nhiều lần)
+//   -> lặp lại bước 1 2 (3.1 với 3.2 e chọn 1 trong 2 nhé)
+// 3.2- để 1 số data dạng mảng vào, còn cách truyền data vào thì ddeeer a xử
+//   }
+// ]
+
 function HomePage() {
+  // const buttonRef = useRef(null);
   return (
     <div className="App">
-      { /* TODO: fill here*/ }
       <h2>Danh mục sản phẩm</h2>
       <div className="container">
         <div className="row">
@@ -32,10 +45,11 @@ function HomePage() {
                   Xem ngay
                 </button>
               </div>
-              <a href="#"><Music/></a>
+              <a href="#"><Music /></a>
             </div>
-        </div>
-        <div className="col-md-4 merch">
+          </div>
+
+          <div className="col-md-4 merch">
             <div className="d-flex justify-content-between">
               <div className="content">
                 <h3>Merch</h3>
@@ -49,9 +63,9 @@ function HomePage() {
                   Xem ngay
                 </button>
               </div>
-                <a href="#"><Merch/></a>
-              </div>
+              <a href="#"><Merch /></a>
             </div>
+          </div>
 
           <div className="col-md-4 vinyl">
             <div className="d-flex justify-content-between">
@@ -67,10 +81,11 @@ function HomePage() {
                   Xem ngay
                 </button>
               </div>
-              <a href="#"><Vinyl/></a>
+              <a href="#"><Vinyl /></a>
             </div>
           </div>
         </div>
+
         <div className="row">
           <div className="col-md-6 photobook">
             <div className="d-flex justify-content-between">
@@ -87,7 +102,7 @@ function HomePage() {
                 </button>
               </div>
               <div>
-                <a href="#"><Photobook/></a>
+                <a href="#"><Photobook /></a>
               </div>
             </div>
           </div>
@@ -107,238 +122,139 @@ function HomePage() {
                 </button>
               </div>
               <div>
-                <a href="#"><Lightstick/></a>
+                <a href="#"><Lightstick /></a>
               </div>
             </div>
           </div>
         </div>
 
         <h2>Sale đến "ngất"</h2>
-        <h6>Xem tất cả <a href="#"><Arrow/></a></h6>
-        
+        <h6>Xem tất cả <a href="#"><Arrow /></a></h6>
+
         <div className="product">
           <div className="row">
-            <div className="col-md-3 product_1">
-              <img src={img_product} alt="Product Image" />
-                <div>
-                  <span className="tag">Mới</span>
-                  <span className="tag">Freeship</span>
-                <div className="product-box-title">
-                  <h4>
-                    <a href="" title="j-hope (BTS) 'Jack In The Box' (HOPE Edition)">
-                      j-hope (BTS) 'Jack In The Box' (HOPE Edition)
-                    </a>
-                  </h4>
-                </div>
-              <a href="#"><Star /></a> <a href="#"><Star /></a> <a href="#"><Star /></a> <a href="#"><Star /></a> <a href="#"><Star /></a> (12)
-              <div className="product-box-price d-flex align-items-center">
-                <div className="price-sale">
-                  <span className="price-inner">400.000₫</span>
-                </div>
-                <del className="price-del">500.000₫</del>
-              </div>
-              </div>
-            </div>
+          <HomepageProductItem
+              data={
+                {
+                  product_name: "j-hope (BTS) 'Jack In The Box' (HOPE Edition)",
+                  discount_price: 400.000,
+                  sell_price: 500.000,
+                  img_product: img_product
+                }}
+              onClickHandler={() => { }} />
+            <HomepageProductItem  // them 1 item hehe ma e nho hoi nay a co gui cai vong for k
+              data={
+                {
+                  product_name: "j-hope (BTS) 'Jack In The Box' (HOPE Edition)",
+                  discount_price: 400000,
+                  sell_price: 500000,
+                  img_product: img_product
+                }}
+              onClickHandler={() => { }} />
 
-            <div className="col-md-3 product_2">
-              <img src={img_product} alt="Product Image" />
-                <div>
-                  <span className="tag">Mới</span>
-                  <span className="tag">Freeship</span>
-                <div className="product-box-title">
-                  <h4>
-                    <a href="" title="j-hope (BTS) 'Jack In The Box' (HOPE Edition)">
-                      j-hope (BTS) 'Jack In The Box' (HOPE Edition)
-                    </a>
-                  </h4>
-                </div>
-              <a href="#"><Star /></a> <a href="#"><Star /></a> <a href="#"><Star /></a> <a href="#"><Star /></a> <a href="#"><Star /></a> (12)
-              <div className="product-box-price d-flex align-items-center">
-                <div className="price-sale">
-                  <span className="price-inner">400.000₫</span>
-                </div>
-                <del className="price-del">500.000₫</del>
-              </div>
-              </div>
-            </div>
+            <HomepageProductItem  // them 1 item hehe ma e nho hoi nay a co gui cai vong for k
+              data={
+                {
+                  product_name: "j-hope (BTS) 'Jack In The Box' (HOPE Edition)",
+                  discount_price: 400000,
+                  sell_price: 500000,
+                  img_product: img_product
+                }}
+              onClickHandler={() => { }} />
 
-            <div className="col-md-3 product_3">
-              <img src={img_product} alt="Product Image" />
-                <div>
-                  <span className="tag">Mới</span>
-                  <span className="tag">Freeship</span>
-                <div className="product-box-title">
-                  <h4>
-                    <a href="" title="j-hope (BTS) 'Jack In The Box' (HOPE Edition)">
-                      j-hope (BTS) 'Jack In The Box' (HOPE Edition)
-                    </a>
-                  </h4>
-                </div>
-              <a href="#"><Star /></a> <a href="#"><Star /></a> <a href="#"><Star /></a> <a href="#"><Star /></a> <a href="#"><Star /></a> (12)
-              <div className="product-box-price d-flex align-items-center">
-                <div className="price-sale">
-                  <span className="price-inner">400.000₫</span>
-                </div>
-                <del className="price-del">500.000₫</del>
-              </div>
-              </div>
-            </div>
-
-            <div className="col-md-3 product_4">
-              <img src={img_product} alt="Product Image" />
-                <div>
-                  <span className="tag">Mới</span>
-                  <span className="tag">Freeship</span>
-                <div className="product-box-title">
-                  <h4>
-                    <a href="" title="j-hope (BTS) 'Jack In The Box' (HOPE Edition)">
-                      j-hope (BTS) 'Jack In The Box' (HOPE Edition)
-                    </a>
-                  </h4>
-                </div>
-              <a href="#"><Star /></a> <a href="#"><Star /></a> <a href="#"><Star /></a> <a href="#"><Star /></a> <a href="#"><Star /></a> (12)
-              <div className="product-box-price d-flex align-items-center">
-                <div className="price-sale">
-                  <span className="price-inner">400.000₫</span>
-                </div>
-                  <del className="price-del">500.000₫</del>
-                </div>
-              </div>
-            </div>
+            <HomepageProductItem  // them 1 item hehe ma e nho hoi nay a co gui cai vong for k
+              data={
+                {
+                  product_name: "j-hope (BTS) 'Jack In The Box' (HOPE Edition)",
+                  discount_price: 400000,
+                  sell_price: 500000,
+                  img_product: img_product
+                }}
+              onClickHandler={() => { }} />
           </div>
         </div>
 
 
         <h2>Mới ra mắt. Nóng cả tay</h2>
-        <h6>Xem tất cả <a href="#"><Arrow/></a></h6>
+        <h6>Xem tất cả <a href="#"><Arrow /></a></h6>
 
         <div className="product">
+          <div className="row" >
+            <HomepageProductItem
+              data={
+                {
+                  product_name: "j-hope (BTS) 'Jack In The Box' (HOPE Edition)",
+                  discount_price: 400.000,
+                  sell_price: 500.000,
+                  img_product: img_product
+                }}
+              onClickHandler={() => { }} />
+            <HomepageProductItem  // them 1 item hehe ma e nho hoi nay a co gui cai vong for k
+              data={
+                {
+                  product_name: "j-hope (BTS) 'Jack In The Box' (HOPE Edition)",
+                  discount_price: 400000,
+                  sell_price: 500000,
+                  img_product: img_product
+                }}
+              onClickHandler={() => { }} />
+
+            <HomepageProductItem  // them 1 item hehe ma e nho hoi nay a co gui cai vong for k
+              data={
+                {
+                  product_name: "j-hope (BTS) 'Jack In The Box' (HOPE Edition)",
+                  discount_price: 400000,
+                  sell_price: 500000,
+                  img_product: img_product
+                }}
+              onClickHandler={() => { }} />
+
+            <HomepageProductItem  // them 1 item hehe ma e nho hoi nay a co gui cai vong for k
+              data={
+                {
+                  product_name: "j-hope (BTS) 'Jack In The Box' (HOPE Edition)",
+                  discount_price: 400000,
+                  sell_price: 500000,
+                  img_product: img_product
+                }}
+              onClickHandler={() => { }} />
+
+          </div>
+        </div>
+
+        <h2>Idol siêu chất, nổi bần bật</h2>
+        <h6>Xem tất cả nghệ sĩ<a href="#"><Arrow /></a></h6>
+        <div className="artist">
           <div className="row">
-            <div className="col-md-3 product_1">
-              <img src={img_product} alt="Product Image" />
-                <div>
-                  <span className="tag">Mới</span>
-                  <span className="tag">Freeship</span>
-                <div className="product-box-title">
-                  <h4>
-                    <a href="" title="j-hope (BTS) 'Jack In The Box' (HOPE Edition)">
-                      j-hope (BTS) 'Jack In The Box' (HOPE Edition)
-                    </a>
-                  </h4>
+            <div className="col-6">
+              <div className="blackpink row">
+                <div className="col-md-6">
+                  <img src={img_blackpink} alt="Blackpink Image" />
                 </div>
-              <a href="#"><Star /></a> <a href="#"><Star /></a> <a href="#"><Star /></a> <a href="#"><Star /></a> <a href="#"><Star /></a> (12)
-              <div className="product-box-price d-flex align-items-center">
-                <div className="price-sale">
-                  <span className="price-inner">400.000₫</span>
+                <div className="col-6">
+                  <img src={logo_blackpink} alt="Blackpink Logo" />
+                  <h3>BLACKPINK</h3>
+                  <h6>Xem sản phẩm<a href="#"><Arrow /></a></h6>
                 </div>
-                <del className="price-del">500.000₫</del>
-              </div>
               </div>
             </div>
 
-            <div className="col-md-3 product_2">
-              <img src={img_product} alt="Product Image" />
-                <div>
-                  <span className="tag">Mới</span>
-                  <span className="tag">Freeship</span>
-                <div className="product-box-title">
-                  <h4>
-                    <a href="" title="j-hope (BTS) 'Jack In The Box' (HOPE Edition)">
-                      j-hope (BTS) 'Jack In The Box' (HOPE Edition)
-                    </a>
-                  </h4>
+            <div className="col-md-6">
+              <div className="blackpink row">
+                <div className="col-md-6">
+                  {/* Left column for img_blackpink */}
+                  <img src={img_blackpink} alt="Blackpink Image" />
                 </div>
-              <a href="#"><Star /></a> <a href="#"><Star /></a> <a href="#"><Star /></a> <a href="#"><Star /></a> <a href="#"><Star /></a> (12)
-              <div className="product-box-price d-flex align-items-center">
-                <div className="price-sale">
-                  <span className="price-inner">400.000₫</span>
-                </div>
-                <del className="price-del">500.000₫</del>
-              </div>
-              </div>
-            </div>
-
-            <div className="col-md-3 product_3">
-              <img src={img_product} alt="Product Image" />
-                <div>
-                  <span className="tag">Mới</span>
-                  <span className="tag">Freeship</span>
-                <div className="product-box-title">
-                  <h4>
-                    <a href="" title="j-hope (BTS) 'Jack In The Box' (HOPE Edition)">
-                      j-hope (BTS) 'Jack In The Box' (HOPE Edition)
-                    </a>
-                  </h4>
-                </div>
-              <a href="#"><Star /></a> <a href="#"><Star /></a> <a href="#"><Star /></a> <a href="#"><Star /></a> <a href="#"><Star /></a> (12)
-              <div className="product-box-price d-flex align-items-center">
-                <div className="price-sale">
-                  <span className="price-inner">400.000₫</span>
-                </div>
-                <del className="price-del">500.000₫</del>
-              </div>
-              </div>
-            </div>
-
-            <div className="col-md-3 product_4">
-              <img src={img_product} alt="Product Image" />
-                <div>
-                  <span className="tag">Mới</span>
-                  <span className="tag">Freeship</span>
-                <div className="product-box-title">
-                  <h4>
-                    <a href="" title="j-hope (BTS) 'Jack In The Box' (HOPE Edition)">
-                      j-hope (BTS) 'Jack In The Box' (HOPE Edition)
-                    </a>
-                  </h4>
-                </div>
-              <a href="#"><Star /></a> <a href="#"><Star /></a> <a href="#"><Star /></a> <a href="#"><Star /></a> <a href="#"><Star /></a> (12)
-              <div className="product-box-price d-flex align-items-center">
-                <div className="price-sale">
-                  <span className="price-inner">400.000₫</span>
-                </div>
-                  <del className="price-del">500.000₫</del>
+                <div className="col-md-6">
+                  {/* Right column for logo_blackpink, h3, h6 */}
+                  <img src={logo_blackpink} alt="Blackpink Logo" />
+                  <h3>BLACKPINK</h3>
+                  <h6>Xem sản phẩm<a href="#"><Arrow /></a></h6>
                 </div>
               </div>
             </div>
           </div>
         </div>
-
-        <h2>Idol siêu chất, nổi bần bật</h2>
-        <h6>Xem tất cả nghệ sĩ<a href="#"><Arrow/></a></h6>
-        <div className = "artist">
-          <div className="row">
-            <div className="col-md-6">
-              <div className="blackpink row">
-                <div className="col-md-6">
-                  {/* Left column for img_blackpink */}
-                  <img src={img_blackpink} alt="Blackpink Image" />
-                </div>
-                <div className="col-md-6">
-                  {/* Right column for logo_blackpink, h3, h6 */}
-                  <img src={logo_blackpink} alt="Blackpink Logo" />
-                  <h3>BLACKPINK</h3>
-                  <h6>Xem sản phẩm<a href="#"><Arrow/></a></h6>
-                </div>
-              </div>
-            </div>
-
-            <div className="col-md-6">
-              <div className="blackpink row">
-                <div className="col-md-6">
-                  {/* Left column for img_blackpink */}
-                  <img src={img_blackpink} alt="Blackpink Image" />
-                </div>
-                <div className="col-md-6">
-                  {/* Right column for logo_blackpink, h3, h6 */}
-                  <img src={logo_blackpink} alt="Blackpink Logo" />
-                  <h3>BLACKPINK</h3>
-                  <h6>Xem sản phẩm<a href="#"><Arrow/></a></h6>
-                </div>
-              </div>
-            </div>
-        </div>    
-        </div>  
       </div>
     </div>
   );
