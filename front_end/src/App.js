e1cbb199b5740dcf988a5e087df6b012d2ec3771
khanhha112003 <102938158+khanhha112003@users.noreply.github.com>
2023-12-26 09:29:04 +0700

    cart chưa xong

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,15 +1,15 @@
 import Footer from './components/Footer/Footer';
 import Navbar from './components/Navbar/Navbar';
-import { HomePage } from './screens/HomePage/HomePage'
+// import { HomePage } from './screens/HomePage/HomePage'
 // import { ProductDetail } from './screens/ProductDetail'
-// import { Cart } from './screens/Cart'
+import { Cart } from './screens/Cart'
 function App() {
   return (
     <div className="App">
        <Navbar></Navbar>
        {/* <ProductDetail></ProductDetail> */}
-       {/* <Cart></Cart> */}
-       <HomePage></HomePage>
+       <Cart></Cart> 
+       {/* <HomePage></HomePage> */}
       <Footer></Footer>
     </div>
   );
