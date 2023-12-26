d279d0b22437e3706419eadfc4e08e4329441e58
thalusooissm <lucnt21411@st.uel.edu.vn>
2023-12-26 14:33:59 +0700

    Footer and About us

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -5,6 +5,7 @@ import { ArtistList }  from './screens/Artist/Artist';
 import { SignIn } from './screens/SignIn/SignIn';
 import { SignUp } from './screens/SignUp/SIgnUp';
 import { HomePage } from './screens/HomePage/HomePage';
+import {ProductDetail} from './screens/ProductDetail.js'
 
 // import { HomePage } from './screens/HomePage'
 
@@ -12,11 +13,12 @@ function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
-      {/* <AboutUs></AboutUs> */}
+      <AboutUs></AboutUs>
       {/* <ArtistList></ArtistList> */}
       {/* <SignIn></SignIn> */}
-      <SignUp></SignUp>
-      {/* <HomePage></HomePage> */}
+      {/* <SignUp></SignUp> */}
+      <HomePage></HomePage>
+      <ProductDetail></ProductDetail>
       <Footer></Footer>
     </div>
   );
--- front_end/src/components/Footer/Footer.js
+++ front_end/src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { ReactComponent as LogoIcon } from './icons/logo.svg';
 import { ReactComponent as PhoneIcon } from './icons/icon_phone.svg';
-import { ReactComponent as ApartmentIcon } from './icons/icon_house.svg';
+import { ReactComponent as ApartmentIcon } from './icons/domain.svg';
 import { ReactComponent as MailIcon } from './icons/icon_mail.svg';
 import { ReactComponent as YoutubeIcon } from './icons/icon_youtube.svg';
 import { ReactComponent as InstagramIcon } from './icons/icon_insta.svg';
@@ -11,91 +11,84 @@ import { ReactComponent as FacebookIcon } from './icons/icon_fb.svg';
 import { ReactComponent as TiktokIcon } from './icons/icon_tiktok.svg';
 function Footer() {
   return (
-    <div class="container">
-        <div class="row">
-            <div class="footer-line">
+    <div className="container">
+        <div className="row">
+            <div className="footer-line">
                 <hr/>
             </div>
         </div>
-        <div class="row">
-            <div class="col-sm-6 col-md-6 col-lg-3 col-xl-3">
+        <div className="row">
+            <div className="col-sm-6 col-md-6 col-lg-3 col-xl-3">
                 <a href="#">
-                    <LogoIcon class="img-icon"/>
+                    <LogoIcon className="img-icon"/>
                 </a>
-                <ul class="link-list">
-                    <li><a href="#"><span class="label-s">Về chúng tôi</span></a></li>
-                    <li><a href="#"><span class="label-s">Nhà cung ứng</span></a></li>
-                    <li><a href="#"><span class="label-s">ESG</span></a></li>
-                    <li><a href="#"><span class="label-s">Chứng nhận</span></a></li>
+                <ul className="link-list">
+                    <li><a href="#"><span className="label-m">Về chúng tôi</span></a></li>
+                    <li><a href="#"><span className="label-m">Nhà cung ứng</span></a></li>
+                    <li><a href="#"><span className="label-m">ESG</span></a></li>
+                    <li><a href="#"><span className="label-m">Chứng nhận</span></a></li>
                 </ul>
             </div>
-            <div class="col-sm-6 col-md-6 col-lg-3 col-xl-3">
-                <div class="footer-head"><span class="link-list-title label-xl">Sản phẩm</span></div>
-                <ul class="link-list">
-                    <li><a href="#"><span class="label-s">Album</span></a></li>
-                    <li><a href="#"><span class="label-s">Merch</span></a></li>
-                    <li><a href="#"><span class="label-s">Photobook</span></a></li>
-                    <li><a href="#"><span class="label-s">Vinyl</span></a></li>
-                    <li><a href="#"><span class="label-s">Lightstick</span></a></li>
+            <div className="col-sm-6 col-md-6 col-lg-3 col-xl-3">
+                <div className="footer-head"><span className="link-list-title label-xl">Sản phẩm</span></div>
+                <ul className="link-list">
+                    <li><a href="#"><span className="label-m">Album</span></a></li>
+                    <li><a href="#"><span className="label-m">Merch</span></a></li>
+                    <li><a href="#"><span className="label-m">Photobook</span></a></li>
+                    <li><a href="#"><span className="label-m">Vinyl</span></a></li>
+                    <li><a href="#"><span className="label-m">Lightstick</span></a></li>
                 </ul>
             </div>
-            <div class="col-sm-6 col-md-6 col-lg-3 col-xl-3">
-                <div class="footer-head"><span class="link-list-title label-xl">Chính sách</span></div>
-                <ul class="link-list">
-                    <li><a href="#"><span class="label-s">Chính sách mua hàng</span></a></li>
-                    <li><a href="#"><span class="label-s">Chính sách giao hàng</span></a></li>
-                    <li><a href="#"><span class="label-s">Chính sách thanh toán</span></a></li>
-                    <li><a href="#"><span class="label-s">Dữ liệu cá nhân</span></a></li>
+            <div className="col-sm-6 col-md-6 col-lg-3 col-xl-3">
+                <div className="footer-head"><span className="link-list-title label-xl">Chính sách</span></div>
+                <ul className="link-list">
+                    <li><a href="#"><span className="label-m">Chính sách mua hàng</span></a></li>
+                    <li><a href="#"><span className="label-m">Chính sách giao hàng</span></a></li>
+                    <li><a href="#"><span className="label-m">Chính sách thanh toán</span></a></li>
+                    <li><a href="#"><span className="label-m">Dữ liệu cá nhân</span></a></li>
                 </ul>
             </div>
-            <div class="col-sm-6 col-md-6 col-lg-3 col-xl-3 footer-col">
-                <div class="footer-head"><span class="link-list-title label-xl">Liên hệ</span></div>
-                <ul class="link-list">
-                    <li><a href="#"><div class="label-link-icon">
-                        <PhoneIcon/>
-                        {/* <img src="public\images\ad-units.svg" alt="Phone"/> */}
-                        <span class="label-s">0904944193</span>
+            <div className="col-sm-6 col-md-6 col-lg-3 col-xl-3 footer-col">
+                <div className="footer-head"><span className="link-list-title label-xl">Liên hệ</span></div>
+                <ul className="link-list">
+                    <li><a href="#"><div className="label-link-icon">
+                        <PhoneIcon className='label-icon'/>
+                        <span className="label-m">0904944193</span>
                     </div></a></li>
-                    <li><a href="#"><div class="label-link-icon">
-                        {/* <img src="public\images\drafts.svg" alt="Mail"/> */}
-                        <MailIcon/>
-                        <span class="label-s">pop4u@gmail.com</span>
+                    <li><a href="#"><div className="label-link-icon">
+                        <MailIcon className='label-icon'/>
+                        <span className="label-m">pop4u@gmail.com</span>
                     </div></a></li>
-                    <li><a href="#"><div class="label-link-icon">
-                        {/* <img src="public\images\apartment.svg" alt="Mail"/> */}
-                        <ApartmentIcon/>
-                        <span class="label-s">669 QL1A, khu phố 3, Thủ Đức, Thành phố Hồ Chí Minh</span>
+                    <li><a href="#"><div className="label-link-icon">
+                        <ApartmentIcon className='label-icon'/>
+                        <span className="label-m">669 QL1A, KP.3, Thủ Đức, TP.HCM</span>
                     </div></a></li>
                 </ul>
             </div>
         </div>
-        <div class="row">
-            <div class="footer-line">
+        <div className="row">
+            <div className="footer-line">
                 <hr/>
             </div>
         </div>
-        <div class="row justify-content-between">
-            <div class="col-sm-12 col-md-6 col-lg-6 col-xl-6 footer-list-social-icon">
+        <div className="row justify-content-between">
+            <div className="col-sm-12 col-md-6 col-lg-6 col-xl-6 footer-list-social-icon">
                 <a href="#">
-                    {/* <img src="public/images/icon-facebook-icon.svg" alt="Facebook" class="footer-social-icon"/> */}
-                    <FacebookIcon class="icon-social" />
+                    <FacebookIcon className="icon-social" />
                 </a>
                 <a href="#">
-                    {/* <img src="public/images/icon-youtube-round-2.svg" alt="Facebook" class="footer-social-icon"/> */}
-                    <YoutubeIcon class="icon-social"/>
+                    <YoutubeIcon className="icon-social"/>
                 </a>
                 <a href="#">
-                    {/* <img src="public/images/icon-instagram-icon.svg" alt="Facebook" class="footer-social-icon"/> */}
-                    <InstagramIcon class="icon-social"/>
+                    <InstagramIcon className="icon-social"/>
                 </a>
                 <a href="#">
-                    {/* <img src="public/images/icon-tik-tok.svg" alt="Facebook" class="footer-social-icon"/> */}
-                    <TiktokIcon class="icon-social"/>
+                    <TiktokIcon className="icon-social"/>
                 </a>
             </div>
-            <div class="col-sm-12 col-md-6 col-lg-6 col-xl-6">
-                <div class="copy-right">
-                    <span class="body-small">© Bản quyền thuộc về Pop4u 2023.</span>
+            <div className="col-sm-12 col-md-6 col-lg-6 col-xl-6">
+                <div className="copy-right">
+                    <span className="body-small">© Bản quyền thuộc về Pop4u 2023.</span>
                 </div>
             </div>
         </div>
