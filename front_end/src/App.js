9b49f540b2bb67e5920d8f356a20a0ea7f14e4db
xuntheo <thieuntx21411@st.uel.edu.vn>
2023-12-27 09:16:13 +0700

    test

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -4,14 +4,16 @@ import { HomePage } from './screens/HomePage/HomePage'
 // import { ProductDetail } from './screens/ProductDetail'
 // import { Cart } from './screens/Cart'
 import {ProductPage} from './screens/ProductPage/ProductPage'
+import {Artistlist} from './screens/Artist/Artist'
 function App() {
   return (
     <div className="App">
        <Navbar></Navbar>
        {/* <ProductDetail></ProductDetail> */}
        {/* <Cart></Cart> */}
-       <HomePage></HomePage>
-       <ProductPage></ProductPage>
+       {/* <HomePage></HomePage> */}
+       {/* <ProductPage></ProductPage> */}
+       <ArtistList></ArtistList>
       <Footer></Footer>
     </div>
   );
