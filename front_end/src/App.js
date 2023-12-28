38d33c8d5f45a4af2044dd5ba07cbe88bcc45145
ThuyBumr <anhthuylqd@gmail.com>
2023-12-29 01:34:54 +0700

    add routing

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,27 +1,36 @@
 import Footer from './components/Footer/Footer';
 import Navbar from './components/Navbar/Navbar';
 import { AboutUs } from './screens/AboutUs/AboutUs';
-import { ArtistList }  from './screens/Artist/Artist';
+import { ArtistList } from './screens/Artist/Artist';
 import { SignIn } from './screens/SignIn/SignIn';
 import { SignUp } from './screens/SignUp/SIgnUp';
 import { HomePage } from './screens/HomePage/HomePage';
 import { ProductDetail } from './screens/ProductDetail';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useParams
+} from "react-router-dom";
 // import { ProductDetail } from './screens/ProductDetail'
 // import { Cart } from './screens/Cart'
 function App() {
   return (
-    <div className="App">
-      <Navbar></Navbar>
-      {/* <AboutUs></AboutUs> */}
-      {/* <ArtistList></ArtistList> */}
-      {/* <SignIn></SignIn> */}
-      {/* <SignUp></SignUp> */}
-      <HomePage></HomePage>
-      {/* <ProductDetail></ProductDetail> */}
-       {/* <ProductDetail></ProductDetail> */}
-       {/* <Cart></Cart> */}
-      <Footer/>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar/>
+        <Routes>
+          <Route exact path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/artists" element={<ArtistList />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signun" element={<SignUp />} />
+          {/* <Route path="/products" children={<ProductDetail />} /> */}
+        </Routes>
+        <Footer/>
+      </div>
+    </Router>
+
   );
 }
 
