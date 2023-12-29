b426cc7581ecdd86c2cee1f986ccded4cde7837f
khanhha112003 <102938158+khanhha112003@users.noreply.github.com>
2023-12-29 21:18:36 +0700

    pull

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,44 +1,37 @@
 import Footer from './components/Footer/Footer';
-import Navbar from './components/Navbar/Navbar';
+import CustomNavbar from './components/Navbar/Navbar';
+import { useEffect } from "react";
 import { AboutUs } from './screens/AboutUs/AboutUs';
 import { ArtistList } from './screens/Artist/Artist';
 import { SignIn } from './screens/SignIn/SignIn';
 import { SignUp } from './screens/SignUp/SIgnUp';
 import { HomePage } from './screens/HomePage/HomePage';
-<<<<<<< HEAD
-import { ProductDetail } from './screens/ProductDetail';
-// import { ProductDetail } from './screens/ProductDetail'
-// import { Cart } from './screens/Cart'
-import { ProductListAdmin } from './screens/Admin/Product/ProductListAdmin'
-import { AddProduct } from './screens/Admin/Product/AddProduct';
-function App() {
-  return (
-    <div className="App">
-      <Navbar></Navbar>
-      {/* <AboutUs></AboutUs> */}
-      {/* <ArtistList></ArtistList> */}
-      {/* <SignIn></SignIn> */}
-      {/* <SignUp></SignUp> */}
-      {/* <HomePage></HomePage> */}
-      {/* <ProductDetail></ProductDetail> */}
-       {/* <ProductDetail></ProductDetail> */}
-       {/* <Cart></Cart> */}
-       {/* <ProductListAdmin></ProductListAdmin> */}
-       {/* <AddProduct></AddProduct> */}
-    </div>
-=======
 import { ProductDetail } from './screens/ProductDetail/ProductDetail';
+import { ProductPage } from './screens/ProductPage/ProductPage';
 import {
   BrowserRouter as Router,
   Routes,
   Route,
-  useParams
+  useLocation
 } from "react-router-dom";
+
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <div className="App">
-        <Navbar/>
+        <CustomNavbar />
         <Routes>
           <Route exact path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutUs />} />
@@ -46,12 +39,15 @@ function App() {
           <Route path="/signin" element={<SignIn />} />
           <Route path="/signun" element={<SignUp />} />
           <Route path="/product_detail" element={<ProductDetail />} />
+          <Route path="/product_list">
+            <Route path=":sort" element={<ProductPage />} />
+            <Route path="" element={<ProductPage />} />
+          </Route>
         </Routes>
-        <Footer/>
+        <Footer />
       </div>
     </Router>
 
->>>>>>> main
   );
 }
 
