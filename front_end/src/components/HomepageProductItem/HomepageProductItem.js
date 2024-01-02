2fcc48d1fcbc62319e9ae71e9ac843aa37b13c1b
ThuyBumr <anhthuylqd@gmail.com>
2024-01-02 21:23:49 +0700

    fix db

--- front_end/src/components/HomepageProductItem/HomepageProductItem.js
+++ front_end/src/components/HomepageProductItem/HomepageProductItem.js
@@ -4,6 +4,7 @@ import "./HomepageProductItem.css";
 import RatingBar from '../RatingBar/RatingBar';
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
+import { BadgeList } from '../BadgeList/BadgeList';
 
 import { Link } from 'react-router-dom'
 
@@ -21,10 +22,12 @@ const HomepageProductItem = ({ data }) => {
         </div>
     );
 
+    console.log(data);
+
     return (
         <Link to="/product_detail" style={{ width: '100%', height: '100%', cursor: 'pointer' }}>
         <Card style={{ width: '100%', height: '100%', padding: '10px 0'}}>
-            <Card.Img style={{height: 180, objectFit: 'contain'}} variant="top" src={data.img_product} />
+            <Card.Img style={{height: 180, objectFit: 'contain'}} variant="top" src={data.list_product_image[0]} />
             <Card.Body className='product_title' style={{paddingTop: 5, paddingBottom: 5}}>
                 <Card.Title>
                     {data.product_name}
@@ -32,15 +35,12 @@ const HomepageProductItem = ({ data }) => {
             </Card.Body>
             <ListGroup style={{border: 0, marginTop: 'auto'}}>
                 <ListGroup.Item style={{border: 0, paddingTop: 5, paddingBottom: 5}}>
-                    {
-                        data.special_badge !== null
-                        ? ( 
-                            data.special_badge.map((item, index) => (
-                                <span key={index} className="tag">{item}</span>
-                            ))
-                        ) 
-                        : (<div></div>)
-                    }
+                    <BadgeList data={{
+                        is_new: data.is_new,
+                        is_hot: data.is_hot,
+                        is_freeship: data.is_freeship,
+                        is_sale: data.is_sale
+                    }} />
                 </ListGroup.Item>
                 <ListGroup.Item style={{border: 0, paddingTop: 5, paddingBottom: 5}}>
                     <RatingBar 
