c45ca16d5f52d4a5346c02b9d496b350c7ba2a53
ThuyBumr <anhthuylqd@gmail.com>
2024-01-03 01:59:56 +0700

    product list page add static data

--- front_end/src/screens/ProductList/ProductList.js
+++ front_end/src/screens/ProductList/ProductList.js
@@ -1,17 +1,29 @@
 import React, { useState } from "react";
+import { useParams } from 'react-router-dom';
 import './style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import banner_tet from './images/banner_tet.png'
-import { ReactComponent as Star } from './images/grade.svg'
-import img_album from './images/img_album.png'
-import Carousel from 'react-bootstrap/Carousel';
 import HomepageProductItem from "../../components/HomepageProductItem/HomepageProductItem";
 import data from './data.js'
-// import Category from "./Category";
-// import Card from "./Card/Card";
+import { items } from './list_product.js'
+import { Container, Row, Col, Pagination } from 'react-bootstrap';
+
+const itemsPerPage = 16;
 
 function ProductList() {
     const [index, setIndex] = useState(0);
+    const { sort } = useParams();
+
+    const [currentPage, setCurrentPage] = useState(1);
+
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+    const currentItems = items.slice(indexOfFirstItem, indexOfLastItem);
+
+    const totalPages = Math.ceil(items.length / itemsPerPage);
+
+    const handlePageChange = (page) => {
+        setCurrentPage(page);
+    };
 
     const handleSelect = (selectedIndex) => {
         setIndex(selectedIndex);
@@ -19,28 +31,28 @@ function ProductList() {
     const [filterName, setFilterName] = useState('');
     const [searchTimeout, setSearchTimeout] = useState(null);
     const [filteredData, setFilteredData] = useState(data);
-  
+
     const handleFilterChange = event => {
-      const inputValue = event.target.value;
-      setFilterName(inputValue);
-  
-      // Clear previous timeout
-      if (searchTimeout) {
-        clearTimeout(searchTimeout);
-      }
-  
-      // Set new timeout for 2 seconds
-      const newTimeout = setTimeout(() => {
-        const newFilteredData = data.filter(artist =>
-          artist.name.toLowerCase().includes(inputValue.toLowerCase())
-        );
-        setFilteredData(newFilteredData);
-      }, 150);
-  
-      // Save the new timeout ID
-      setSearchTimeout(newTimeout);
+        const inputValue = event.target.value;
+        setFilterName(inputValue);
+
+        // Clear previous timeout
+        if (searchTimeout) {
+            clearTimeout(searchTimeout);
+        }
+
+        // Set new timeout for 2 seconds
+        const newTimeout = setTimeout(() => {
+            const newFilteredData = data.filter(artist =>
+                artist.name.toLowerCase().includes(inputValue.toLowerCase())
+            );
+            setFilteredData(newFilteredData);
+        }, 150);
+
+        // Save the new timeout ID
+        setSearchTimeout(newTimeout);
     };
-    
+
     return (
 
 
@@ -52,32 +64,6 @@ function ProductList() {
                     </div>
                 </div>
             </div>
-                   
-                    {/* <Carousel activeIndex={index} onSelect={handleSelect}>
-                        <Carousel.Item>
-                            <div className='banner-artist col-sm-12 col-md-12 col-lg-12 col-xl-12 justify-content-right justify-content-md-center'>
-                                <Carousel.Caption>
-                                    <span className='head3'>Sản phẩm của chúng tôi</span>
-                                </Carousel.Caption>
-                            </div>
-                        </Carousel.Item>
-                        <Carousel.Item>
-                            <img className="banner_tet d-block w-100" src={banner_tet} alt='second_banner' />
-                            <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </Carousel.Caption>
-                        </Carousel.Item>
-                        <Carousel.Item>
-                <img className="d-block w-100" src={banner_tet} alt='banner' />
-                    <Carousel.Caption>
-                    <h3>Third slide label</h3>
-                    <p>
-                        Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                    </p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                    </Carousel> */}
 
             <div className="row">
                 <div className='search-product'>
@@ -86,12 +72,12 @@ function ProductList() {
                     </div>
                     <div className="col-sm-12 col-md-12 col-lg-12 col-xl-12 justify-content-right justify-content-md-center">
                         <input
-                        className='label-m'
-                        type="text"
-                        id="filterInput"
-                        value={filterName}
-                        onChange={handleFilterChange}
-                        placeholder="Hãy điền tên sản phẩm vào đây..."
+                            className='label-m'
+                            type="text"
+                            id="filterInput"
+                            value={filterName}
+                            onChange={handleFilterChange}
+                            placeholder="Hãy điền tên sản phẩm vào đây..."
                         />
                     </div>
                 </div>
@@ -202,56 +188,34 @@ function ProductList() {
                 </div>
                 <div className="col-md-9">
 
-                    <div className="col-md-3">
-                        <div className="row">
-                            <div className="product">
-                                <div className="row" >
+                    <Container>
+                        <Row>
+                            {currentItems.map((item, index) => (
+                                <Col key={index} sm={3}>
                                     <HomepageProductItem
-                                        data={
-                                            {
-                                                product_name: "j-hope (BTS) 'Jack In The Box' (HOPE Edition)",
-                                                discount_price: 400000,
-                                                sell_price: 500000,
-                                                img_product: img_album,
-                                                rating_detail: "",
-                                                rating: 4.3,
-                                                special_badge: []
-                                            }}
-                                        onClickHandler={() => { }} />
-                                </div>
-                            </div>
-                        </div>
-
-
-                    </div>
+                                        key={'product' + index}
+                                        data={item}
+                                    />
+                                </Col>
+                            ))}
+                        </Row>
+                        <Row className="mt-3 d-flex justify-content-center">
+                            <Col style={{ display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+                                <Pagination>
+                                    {[...Array(totalPages)].map((_, index) => (
+                                        <Pagination.Item
+                                            key={index + 1}
+                                            active={index + 1 === currentPage}
+                                            onClick={() => handlePageChange(index + 1)}
+                                        >
+                                            {index + 1}
+                                        </Pagination.Item>
+                                    ))}
+                                </Pagination>
+                            </Col>
+                        </Row>
+                    </Container>
                 </div>
-
-
-
-
-
-                {/* <div className="row">
-            <div className="col-sm-12 col-md-12 col-lg-12 col-xl-12 justify-content-right justify-content-md-right">
-                    
-            <div className="product">
-                <div className="row" >
-                <HomepageProductItem
-                data={
-                    {
-                    product_name: "j-hope (BTS) 'Jack In The Box' (HOPE Edition)",
-                    discount_price: 400000,
-                    sell_price: 500000,
-                    img_product: img_album
-                    }}
-                onClickHandler={() => { }} />
-            </div>    
-                    
-                    </div>
-                </div>
-
-                
-            </div> */}
-
             </div>
         </div>
     )
