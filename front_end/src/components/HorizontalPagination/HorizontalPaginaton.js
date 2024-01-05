ae64a00eb509428882879feb6855154b912d600b
ThuyBumr <anhthuylqd@gmail.com>
2024-01-06 00:03:54 +0700

    horizontal pagination responsive

--- front_end/src/components/HorizontalPagination/HorizontalPaginaton.js
+++ front_end/src/components/HorizontalPagination/HorizontalPaginaton.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useLayoutEffect } from "react";
 import { Carousel, Card, Stack } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./style.css";
@@ -6,54 +6,68 @@ import "./style.css";
 import { ReactComponent as CustomNextIcon } from "./icons/arrow_right.svg";
 import { ReactComponent as CustomPrevIcon } from "./icons/arrow_left.svg";
 
+const numberOfMaxItems = 4;
+
 const HorizontalPagination = (
-  { gap, 
-    items, 
-    itemWidth, 
-    itemHeight, 
-    paddingItem, 
-    background_color= 'var(--color-surface-light)' ,
-    innerPadding = 0
-  }) =>  {
+    { gap,
+        items,
+        itemWidth,
+        itemHeight,
+        paddingItem,
+        background_color = 'var(--color-surface-light)',
+        innerPadding = 0
+    }) => {
 
-  const itemsPerPage = 4;
+    const [itemsPerPage, setItemsPerPage] = React.useState(numberOfMaxItems);
 
-  return (
-    <Carousel
-      interval={null}
-      indicators={false}
-      style={{ height: itemHeight, margin: `0 -${gap / 2}px` }} // Set negative margin for gap
-      nextIcon={<CustomNextIcon />} // Use custom SVG for next arrow
-      prevIcon={<CustomPrevIcon />} // Use custom SVG for prev arrow
-    >
-      {Array.from({ length: Math.ceil(items.length / itemsPerPage) }).map((_, index) => (
-        <Carousel.Item 
-          key={index} 
-          style={{ 
-            padding: innerPadding,
-            height: itemHeight, 
-            backgroundColor: background_color, 
-            border: 'none' }}
+    useLayoutEffect(() => {
+        const handleResize = () => {
+            const currentMaxItem = Math.min(Math.floor(window.innerWidth / itemWidth), numberOfMaxItems)
+            setItemsPerPage(currentMaxItem);
+        };
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    return (
+        <Carousel
+            interval={null}
+            indicators={false}
+            style={{ height: itemHeight, margin: `0 -${gap / 2}px` }} // Set negative margin for gap
+            nextIcon={<CustomNextIcon />} // Use custom SVG for next arrow
+            prevIcon={<CustomPrevIcon />} // Use custom SVG for prev arrow
         >
-          <Stack
-            direction="horizontal"
-            className="h-100 justify-content-center align-items-center"
-            gap={gap}
-          >
-            {items
-              .slice(index * itemsPerPage, (index + 1) * itemsPerPage)
-              .map((item, i) => (
-                <Card key={i} style={{ width: itemWidth, margin: paddingItem, padding: 0, height: "100%" }}>
-                  <Card.Body style={{padding: 0}}>
-                    {item}
-                  </Card.Body>
-                </Card>
-              ))}
-          </Stack>
-        </Carousel.Item>
-      ))}
-    </Carousel>
-  );
+            {Array.from({ length: Math.ceil(items.length / itemsPerPage) }).map((_, index) => (
+                <Carousel.Item
+                    key={index}
+                    style={{
+                        padding: innerPadding,
+                        height: itemHeight,
+                        backgroundColor: background_color,
+                        border: 'none'
+                    }}
+                >
+                    <Stack
+                        direction="horizontal"
+                        className="h-100 justify-content-center align-items-center"
+                        gap={gap}
+                    >
+                        {items
+                            .slice(index * itemsPerPage, (index + 1) * itemsPerPage)
+                            .map((item, i) => (
+                                <Card key={i} style={{ width: itemWidth, margin: paddingItem, padding: 0, height: "100%" }}>
+                                    <Card.Body style={{ padding: 0 }}>
+                                        {item}
+                                    </Card.Body>
+                                </Card>
+                            ))}
+                    </Stack>
+                </Carousel.Item>
+            ))}
+        </Carousel>
+    );
 };
 
 export default HorizontalPagination;
