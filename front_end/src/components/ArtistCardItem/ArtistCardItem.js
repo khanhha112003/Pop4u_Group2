9afb2f2333b43a61fe32e515aee26c7e57cf1b0c
thalusooissm <lucnt21411@st.uel.edu.vn>
2024-01-05 11:45:41 +0700

    Edit transition

--- front_end/src/components/ArtistCardItem/ArtistCardItem.js
+++ front_end/src/components/ArtistCardItem/ArtistCardItem.js
@@ -28,60 +28,7 @@ export const ArtistCardItem = ({ data, onMoreInfoClick }) => {
                             <a href={onMoreInfoClick}><span className="label-l">Xem sản phẩm </span><Arrow/></a>
                         </div>
                     </div>
-                    {/* <Card.Body style={{ padding: 0, height: '100%' }}>
-                        <Container style={{ padding: 0, height: '100%' }}>
-                            <Row style={{ padding: 0, height: '100%' }}>
-                                <Col md={6} style={{ padding: 0 }}>
-                                    <div
-                                        style={{
-                                            borderRadius: '20px 0 0 20px',
-                                            overflow: 'hidden',
-                                            height: '100%',
-                                        }}
-                                    >
-                                        <img
-                                            src={data.artist_avatar}
-                                            alt="Your Image"
-                                            style={{
-                                                width: '100%',
-                                                height: '100%',
-                                                objectFit: 'cover',
-                                            }}
-                                        />
-                                    </div>
-                                </Col>
-
-                                <Col md={6} style={{ backgroundColor: '#D8E2FF', padding: '20px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                                    <div style={{ textAlign: 'center' }}>
-                                        <img
-                                            src={data.artist_logo}
-                                            alt="Avatar"
-                                            style={{
-                                                width: '100px',
-                                                height: '100px',
-                                                borderRadius: '50%',
-                                                objectFit: 'cover',
-                                                marginBottom: '10px',
-                                            }}
-                                        />
-
-                                        <h3 style={{ fontSize: '1.5rem', fontWeight: 'normal' }}>
-                                            {data.artist_name}
-                                        </h3>
-
-                                        <h6>
-                                            Xem sản phẩm
-                                            <a href="#" onClick={onMoreInfoClick}>
-                                                <Arrow />
-                                            </a>
-                                        </h6>
-                                    </div>
-                                </Col>
-                            </Row>
-                        </Container>
-                    </Card.Body> */}
                 </div>
-
             </div>
         </div>
     );
