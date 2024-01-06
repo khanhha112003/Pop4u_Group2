8e8e0580eaf75162af50c98b870962ab235d601a
ThuyBumr <anhthuylqd@gmail.com>
2024-01-06 14:44:24 +0700

    artist detail done

--- front_end/src/screens/ProductArtist/ProductArtist.js
+++ front_end/src/screens/ProductArtist/ProductArtist.js
@@ -2,6 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import { useSearchParams, useNavigate } from 'react-router-dom';
 import 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import { ReactComponent as Arrow } from './icons/icon_arrow.svg';
 
 
@@ -18,33 +19,52 @@ function ProductArtist() {
     const navigate = useNavigate();
     const [isLoading, setIsLoading] = useState(false);
     const [isError, setIsError] = useState(false);
-    
-    useEffect(() => {
-        const getArtistRequest = basicGetRequets('/artist/artist_detail', { artist_code: searchParam.get('artist_code') });
-        const getArtistProductRequest = basicGetRequets('/product/product_list', { artist_code: searchParam.get('artist_code') });
-
-        const combineRequest = combineMultipleRequests(getArtistRequest, getArtistProductRequest);
-        combineRequest.then(response => {
-            setContent({artist_data: response[0].data, related_products: response[1].data});
-        }).catch(error => {
-            setIsError(true);
-        }).finally(() => {
-            setIsLoading(false);
-        })
-    },[])
 
     const [content, setContent] = useState(
         {
-            artist_data: {            
+            artist_data: {
                 artist_name: '',
                 artist_code: '',
                 is_hot: false,
-                artist_logo: [],
-                artist_avatar: [],
+                artist_logo: "",
+                artist_avatar: "",
                 artist_description: '',
             },
             related_product: []
         });
+
+    useEffect(() => {
+        const getArtistRequest = basicGetRequets('/artist/artist_detail', { artist_code: searchParam.get('artist_code') });
+        const getArtistProductRequest = basicGetRequets('/product/product_list', { artist_code: searchParam.get('artist_code') });
+
+        const combineRequest = combineMultipleRequests([getArtistRequest, getArtistProductRequest])
+            .then(responses => {
+                var list_product = [];
+                if (responses[1].data.list_product.length > 0) {
+                    list_product = responses[1].data.list_product;
+                }
+                console.log(responses[0].data.artist_avatar)
+                setContent({ artist_data: responses[0].data, related_product: list_product });
+            }).catch(error => {
+                setIsError(true);
+            }).finally(() => {
+                setIsLoading(false);
+            })
+    }, [])
+
+    const styles = {
+        overlayImage: {
+            backgroundImage: `linear-gradient(0deg, rgba(128,128,128,1) 0%, rgba(128,128,128,0.9) 20%, rgba(128,128,128,0.3) 80%, rgba(255,255,255,0) 100%) ,url(${content.artist_data.artist_avatar})`,
+            height: '100vh',
+            backgroundPosition: 'center',
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'cover',
+            display: 'flex',
+            flex: '0 0 auto',
+            width: '100%',
+            alignItems: 'flex-end'
+        },
+    }
 
     if (isError) {
         return <NotFoundPage />
@@ -53,24 +73,19 @@ function ProductArtist() {
     if (isLoading) {
         return <LoadingPage />
     }
-    
+
     return (
         <div className="container">
             <div className="row">
-                <div 
-                    className="col-md-12 col-sm-12" 
-                    style={{
-                        backgroundImage: content.artist_data.artist_avatar,
-                        background: 'rgb(128,128,128)',
-                        background: 'linear-gradient(0deg, rgba(128,128,128,0.7233018207282913) 0%, rgba(255,255,255,0) 47%, rgba(255,255,255,0) 100%)'
-                    }}
-                >
-                    
-                </div>
+                <Row className="m-0">
+                    <Col md={12} sm={12} style={styles.overlayImage}>
+                        <div className="text-light">
+                            <h3>{content.artist_data.artist_name}</h3>
+                            <p>{content.artist_data.artist_description}</p>
+                        </div>
+                    </Col>
+                </Row>
 
-                <div className="col-md-3 product_1">
-                    {content.artist_data.artist_description}
-                </div>
                 {
                     content.related_product.length > 0 && (
                         <div>
