62678dcaeb397b91429630956957439bf9d545e5
khanhha112003 <102938158+khanhha112003@users.noreply.github.com>
2024-01-11 19:17:31 +0700

    SIgnUp chứ

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,6 +1,6 @@
 import { AboutUs } from './screens/AboutUs/AboutUs';
 import { SignIn } from './screens/SignIn/SignIn';
-import { SignUp } from './screens/SignUp/SignUp';
+import { SignUp } from './screens/SignUp/SIgnUp';
 import { HomePage } from './screens/HomePage/HomePage';
 import { BuyPolicy } from './screens/Policy/BuyPolicy';
 import { ShipPolicy } from './screens/Policy/ShipPolicy';
